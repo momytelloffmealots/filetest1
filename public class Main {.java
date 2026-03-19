a073e4d14ee3dbcc48822fe217c808e4ddb5a571
momytelloffmealots <atflm.phu@gmail.com>
2026-03-19 09:44:05 +0700

    Update public class Main {.java
    viet ham for

--- public class Main {.java
+++ public class Main {.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("HELLO");
+        // This is a simple Java program
+        for(int i = 0; i < 5; i++){
+            System.out.println("Count: " + i);
+        }   
     }
 }
